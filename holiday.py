35fb47dbbf572a55c5bce906370c7882f6dc3208
Yves Lai <yves.lai@globalrelay.net>
2026-01-19 01:00:13 -0800

    Fix: Force refresh git index for case sensitivity and init files

--- holiday.py
+++
@@ -1,7 +0,0 @@
-from holiday_reminder import send_canada_holiday_reminder
-
-# from holiday_reminder import push_line_notify
-# push_line_notify("✅ 測試推送 from Messaging API")
-
-if __name__ == "__main__":
-    send_canada_holiday_reminder()
